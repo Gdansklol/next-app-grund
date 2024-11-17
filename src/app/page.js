fc007e244ded87d832ffc5818449f3944c250637
swedenebomi <swedenebomi@gamil.com>
2024-11-17 12:47:11 +0100

    created create folder and page.js file

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+// Content
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <h1>Hello, Nextjs !</h1>
+    <h1>Welcome</h1>
+    <h2>Hello, WEB!</h2>
     </>
   );
 }
